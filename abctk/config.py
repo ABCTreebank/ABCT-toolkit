9de38ce3c3b398e79c1592c74dd73154960fe9af
Nori Hayashi <net@hayashi-lin.net>
2020-12-27 09:07:26 +0000

    further develop the ML component
    - separate the preparation stage and the training stage
    - optimize the reading process of the config file. Allennlp configurations are read off from config.yaml

--- abctk/config.py
+++ abctk/config.py
@@ -5,6 +5,28 @@ import xdg
 
 DIR_RUNTIME = pathlib.Path(__file__).parent / "runtime"
 
+_TOKEN_EMBEDDING_DIM = 200
+_CHAR_EMBEDDING_DIM = 50
+_CHAR_EMBEDDED_DIM = 100
+
+_TOKEN_INDEXERS = {
+    "tokens": {
+        "type": "single_id",
+        "lowercase_tokens": False,
+    },
+    "token_characters": {
+        "type": "characters",
+        "character_tokenizer": {
+            "end_tokens": [
+                "@@PADDING@@",
+                "@@PADDING@@",
+                "@@PADDING@@",
+                "@@PADDING@@",
+            ]
+        }
+    }
+}
+
 CONF_DEFAULT = {
     "bin-sys": {
         "m4": "m4", #"m4",
@@ -22,12 +44,90 @@ CONF_DEFAULT = {
     },
     "max_process_num": len(psutil.Process().cpu_affinity()),
     "ml": {
-        "runtimes": {
-            "wvect": xdg.xdg_data_home() / "ABCT-toolkit/vector-wikija/",
-            # http://www.cl.ecei.tohoku.ac.jp/~m-suzuki/jawiki_vector/ CC-BY-SA 4.0
-            "trainer_settings_jsonnet": DIR_RUNTIME / "supertagger_default.json",
-        },
         "train_test_ratio": 80,
-        "gpu_id": "0",
+        "trainer_settings": {
+            #"vocabulary": {
+               # "directory_path": xdg.xdg_data_home() / "ABCT-toolkit/vocabulary"
+               # given dynamically
+            #},
+            "dataset_reader": {
+                "type": "ja_supertagging_dataset",
+                "lazy": True,
+                "token_indexers": _TOKEN_INDEXERS
+            },
+            "validation_dataset_reader": {
+                "type": "ja_supertagging_dataset",
+                "lazy": True,
+                "token_indexers": _TOKEN_INDEXERS
+            },
+            # "train_data_path": given dynamically
+            # "validation_data_path": given dynamically
+            "model": {
+                "type": "supertagger",
+                "text_field_embedder": {
+                    "token_embedders": {
+                        "tokens": {
+                            "type": "embedding",
+                            "pretrained_file": (
+                                xdg.xdg_data_home() / "ABCT-toolkit/jawiki.entity_vectors.200d.txt"
+                            ),
+                            "embedding_dim": _TOKEN_EMBEDDING_DIM,
+                            "sparse": True
+                        },
+                        "token_characters": {
+                            "type": "character_encoding",
+                            "embedding": {
+                                "embedding_dim": _CHAR_EMBEDDING_DIM,
+                                "sparse": True,
+                                "trainable": True
+                            },
+                            "encoder": {
+                                "type": "cnn",
+                                "embedding_dim": _CHAR_EMBEDDING_DIM,
+                                "num_filters": _CHAR_EMBEDDED_DIM,
+                                "ngram_filter_sizes": [5]
+                            }
+                        }
+                    }
+                },
+                "encoder": {
+                    "type": "lstm",
+                    "input_size": _TOKEN_EMBEDDING_DIM + _CHAR_EMBEDDED_DIM,
+                    "hidden_size": 200,
+                    "num_layers": 2,
+                    "dropout": 0.32,
+                    "bidirectional": True,
+                },
+                "tag_representation_dim": 100,
+                "arc_representation_dim": 100,
+                "dropout": 0.32,
+                "input_dropout": 0.5,
+            },
+            "iterator": {
+                "type": "bucket",
+                "cache_instances": False,
+                "batch_size": 128,
+                "sorting_keys": [
+                    ["words", "num_tokens"]
+                ],
+            },
+            "trainer": {
+                "optimizer": {
+                    "type": "dense_sparse_adam",
+                    "betas": [0.9, 0.9]
+                },
+                "learning_rate_scheduler":{
+                    "type": "reduce_on_plateau",
+                    "mode": "max",
+                    "factor": 0.5,
+                    "patience": 5,
+                },
+                "validation_metric": "+harmonic_mean",
+                "grad_nom": 5,
+                "num_epochs": 100,
+                "patience": 20,
+                "cuda_device": 0,
+            },
+        }
     }
 }
--- abctk/ml/cli.py
+++ abctk/ml/cli.py
@@ -4,40 +4,36 @@ import pathlib
 import sys
 import click
 
-from . import misc
+from . import core
 import abctk.cli_tool as ct
 
-def _get_rand() -> float:
-    """
-        Generate a random float number ranging from 0 to 100.
-        Returns
-        -------
-        num : float
-            A random float number.
-    """
-    import random
+@click.group()
+@click.pass_context
+def cmd_main(ctx): pass
 
-    return random.uniform(0, 100)
-# === END ===
-
-@click.command()
+@cmd_main.command("prepare")
 @click.option(
     "-o", "--output", "--to", "--destination", "destination",
     type = click.Path(file_okay = False, dir_okay = True),
     help = """
         Path to a folder that stores the resulting files.
-        Default to `./result_(timestamp)`.
+        Default to `./ml_prepared_(timestamp)`.
     """
 )
 @click.option(
-    "-o", "--overwrite/--no-overwrite", "can_overwrite",
+    "--overwrite/--no-overwrite", "can_overwrite",
+    default = False,
+)
+@click.option(
+    "--leave-temp", "if_leave_temp",
     default = False,
 )
 @click.pass_context
-def cmd_main(
+def cmd_prepare(
     ctx: click.Context,
     destination: typing.Optional[pathlib.Path],
     can_overwrite: bool,
+    if_leave_temp: bool,
 ):
     r"""
         Generate a parser model (and auxiliariy files) from a treebank.
@@ -45,146 +41,97 @@ def cmd_main(
         The source ABCTreebank must be formatted beforehand as one-line trees
         and given to STDIN.
     """
-    CONFIG_ML: typing.Dict[str, typing.Any] = ctx.obj["CONFIG"]["ml"]
+    try: 
+        # =========================
+        # 1. Prepare the intermediate temp folder
+        # =========================
+        import tempfile 
+        temp_folder: pathlib.Path = pathlib.Path(
+            tempfile.mkdtemp(prefix = "abctk.ml.cli.prepare-")
+        )
 
+        # =========================
+        # 2. Prepare the output folder
+        # =========================
+        dest_folder_root: pathlib.Path 
+        if destination:
+            dest_folder_root = destination
+            # TODO: check if it's empty
+        else:
+            dest_folder_root = ct.create_folder_time(
+                "ml_prepared_", to_make = True
+            ) 
+        # === END IF ===
+
+        core.prepare_ml_data(
+            source_trees = sys.stdin,
+            config = ctx.obj["CONFIG"]["ml"],
+            dir_temp = temp_folder,
+            dir_output = dest_folder_root
+        )
+    finally:
+        if temp_folder and temp_folder.is_dir():
+            if if_leave_temp:
+                sys.stderr.write(f"Temporary file kept at {temp_folder}")
+            else:
+                pass # delete tempfolder
+            # === END IF ===
+        else:
+            pass
+        # === END IF ===
+    # === END TRY ===
+# === END ===
+
+@cmd_main.command("train")
+@click.argument(
+    "source",
+    type = click.Path(file_okay = False, dir_okay = True, exists = True)
+)
+@click.option(
+    "-o", "--output", "--to", "--destination", "destination",
+    type = click.Path(
+        file_okay = False, 
+        dir_okay = True, 
+        resolve_path = True
+    ),
+    help = """
+        Path to a folder that stores the resulting model.
+        Default to `./ml_(timestamp)`.
+    """
+)
+@click.option(
+    "--overwrite/--no-overwrite", "can_overwrite",
+    default = False,
+)
+def cmd_train(
+    source: pathlib.Path, 
+    destination: pathlib.Path,
+    can_overwrite: bool,
+):
     # =========================
-    # 0. Prepare the output folder
+    # Prepare the output folder
     # =========================
+    source_path_name = pathlib.Path(source).name
     dest_folder_root: pathlib.Path 
     if destination:
         dest_folder_root = destination
         # TODO: check if it's empty
     else:
-        dest_folder_root = ct.create_folder_time("result_", to_make = True) 
+        dest_folder_root = ct.create_folder_time(
+            f"ml_{source_path_name}_", to_make = True
+        ) 
     # === END IF ===
 
-    dest_folders: typing.Dict[str, pathlib.Path] = {
-        "root": dest_folder_root,
-        "source": dest_folder_root / "source",
-        "treebank_mod/all": dest_folder_root / "treebank_mod/all",
-        "treebank_mod/train": dest_folder_root / "treebank_mod/train",
-        "treebank_mod/test": dest_folder_root / "treebank_mod/test",
-        "wvect": dest_folder_root / "wvect",
-        "model": dest_folder_root / "model",
-    }
+    import os 
+    os.chdir(source)
 
-    for v in dest_folders.values():
-        v.mkdir(parents = True, exist_ok = True)
-    # === END FOR v ===
-
-    # =========================
-    # 1. Divide trees to the training / test sets
-    # =========================
-    
-    with open(
-        dest_folders["source"] / "all.psd",
-        mode = "w"
-    ) as h_treebank_all, open(
-        dest_folders["source"] / "training.psd",
-        mode = "w"
-    ) as h_treebank_train, open(
-        dest_folders["source"] / "testing.psd",
-        mode = "w"
-    ) as h_treebank_test:
-        # read each tree from STDIN
-        for line in sys.stdin:
-            if _get_rand() < CONFIG_ML["train_test_ratio"]:
-                h_treebank_train.write(line)
-            else:
-                h_treebank_test.write(line)
-            # === END IF ===
-
-            h_treebank_all.write(line)
-        # === END FOR line ===
-
-    # === END WITH h_treebank_all, h_treebank_train, h_treebank_test ===
-    
-    # =========================
-    # 2. Digest the separated treebanks and collect info
-    # =========================
-    digested_treebank: typing.Dict[str, misc.ModderSettings] = {
-        "all": misc.create_mod_treebank(
-            dest_folders["source"] / "all.psd",
-            dest_folders["treebank_mod/all"],
-            mode = "train",
-        ),
-        "train": misc.create_mod_treebank(
-            dest_folders["source"] / "training.psd",
-            dest_folders["treebank_mod/train"],
-            mode = "train",
-        ),
-        "test": misc.create_mod_treebank(
-            dest_folders["source"] / "testing.psd",
-            dest_folders["treebank_mod/test"],
-            mode = "test",
-        )
-    }
-
-    # =========================
-    # 3. Configure the word-vector directory
-    # =========================
-    # Copy the directory to the new folder 
-    import shutil
-
-    shutil.copytree(
-        CONFIG_ML["runtimes"]["wvect"],
-        dest_folders["wvect"],
-        dirs_exist_ok = True,
-    )
-
-    # Overwrite the vocabulary list
-    with open(
-        dest_folders["wvect"] / "head_tags.txt", 
-        mode = "w"
-    ) as h_headtags:
-        h_headtags.write("@@UNKNOWN@@\n")
-        for entry in digested_treebank["all"].targets:
-            h_headtags.write(entry)
-            h_headtags.write("\n")
-        # === END FOR entry ===
-    # === END with h_headtags ===
-
-    # =========================
-    # 4. Configure the trainer
-    # =========================
     import allennlp.common.params as allp
     import allennlp.common.util as allu
     import allennlp.commands.train as allct
     allu.import_submodules("depccg.models.my_allennlp")
 
-    trainer_settings: allp.Params = allp.Params.from_file(
-        CONFIG_ML["runtimes"]["trainer_settings"],
-        ext_vars = {
-            "vocab": str(dest_folders["wvect"]),
-            "train_data": str(
-                dest_folders["treebank_mod/train"] / "traindata.json"
-            ),
-            "test_data": str(
-                dest_folders["treebank_mod/test"] / "testdata.json"
-            ),
-            "gpu": CONFIG_ML["gpu_id"],
-        }
-    )
-
-    # =========================
-    # 5. Dump parser settings
-    # =========================
-    with open(
-        dest_folders["root"] / "config_parser_abc.json",
-        "w"
-    ) as h_parserconf:
-        json.dump(
-            vars(digested_treebank["train"]),
-            h_parserconf,
-            default = str
-        )
-    # === END WITH ===
-
-    # =========================
-    # 6. Execute the trainer
-    # =========================
     allct.train_model(
-        params = trainer_settings,
-        serialization_dir = dest_folders["model"],
+        params = allp.Params.from_file(core.FILES["trainer_settings"]),
+        serialization_dir = dest_folder_root
     )
 # === END ===
