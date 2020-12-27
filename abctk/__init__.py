9cb140addad4d04496a43b913b1ab5b7bcacdce4
Nori Hayashi <net@hayashi-lin.net>
2020-12-27 09:04:22 +0000

    downgrade the python and some packages
    due to the imcompatibility of depccg (1.1.0) and the latest allennlp (>1.0) and that of allennlp (1.2.2) against Python 3.9

--- abctk/__init__.py
+++ abctk/__init__.py
@@ -1,8 +1,14 @@
-import importlib.metadata as im
+import sys
+
+if sys.version_info[:2] < (3, 8):
+    import importlib_metadata as im
+else:
+    import importlib.metadata as im
+# === END IF ===
+
 import logging
 
 # https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-package-version
-
 __version__ = im.version("abctk")
 
 logging.basicConfig(
