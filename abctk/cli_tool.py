c37a7329cd5791f727f4b0595b2812e2ebd382e1
Nori Hayashi <net@hayashi-lin.net>
2021-02-09 08:44:58 +0900

    [refactor] factor out the skeleton of commands that deal with trees/tree files

--- abctk/cli_tool.py
+++ abctk/cli_tool.py
@@ -1,7 +1,13 @@
 import typing
 
 import datetime
+import functools
+import logging
+logger = logging.getLogger(__name__)
 import pathlib
+import sys
+
+import click
 
 def create_folder_time(prefix: str, to_make: bool = True):
     return create_folder_time_multiple(
@@ -15,6 +21,18 @@ def create_folder_time_multiple(
 ) -> typing.Dict[str, pathlib.Path]:
     prefixes_set = set(prefixes)
 
+    if prefixes_set:
+        logger.info(
+            "Going to create the following folders: "
+            + ", ".join(prefixes_set)
+        )
+    else:
+        logger.info(
+            "No folders need to be created automatically"
+        )
+        return {}
+    # === END IF ===
+
     dt_now = datetime.datetime.now()
     dest_folders = dict(
         (pfx, pathlib.Path(pfx + dt_now.isoformat())) 
@@ -22,7 +40,7 @@ def create_folder_time_multiple(
     )
     dest_folders_vals = dest_folders.values()
 
-    while all(f.is_dir() for f in dest_folders_vals):
+    while any(f.is_dir() for f in dest_folders_vals):
         dt_now = datetime.datetime.now()
         dest_folders = dict(
             (pfx, pathlib.Path(pfx + dt_now.isoformat())) 
@@ -117,3 +135,300 @@ class FileList:
         # === END FOR fabs ===
     # === END ===
 # === END CLASS ===
+
+BATCH_PROCESS_ON_TREE = typing.Callable[
+    [
+        click.Context,
+        str, # source_type
+        typing.Optional[str], # destination
+        bool, # can_overwrite
+        bool, # if_gen_intermediate
+        typing.Optional[str] # intermediate_dir
+    ],
+    None
+]
+
+class CmdTemplate_Batch_Process_on_Tree(click.Command):
+    OPTIONS_NORMAL: typing.Tuple[click.Option, ...] = (
+        click.Option(
+            param_decls = [
+                "-i",
+                "--input", "--source", "source_type",
+            ],
+            type = click.Choice(
+                choices = ("rawtrees", "filelist"),
+                case_sensitive = False,
+            ),
+            default = "rawtrees",
+            help = """Designating your data source.
+
+\b
+- `rawtrees' means that input Keyaki trees are provided directry from STDIN.
+- `filelist' indicates that STDIN provides a list of paths to Keyaki tree files.
+
+Default to `rawtrees'. Case insensitive."""
+        ),
+        click.Option(
+            param_decls = [
+                "-o", "--output", "--to", "--destination", "destination",
+            ],
+            type = click.Path(file_okay = False, dir_okay = True),
+            help = """Path to a folder that stores the resulting files.
+Since this program yields results in multiple files in different formats, 
+it is always needed to have a storage folder.
+Default to `./result_(timestamp)`.
+            """
+        ),
+        click.Option(
+            param_decls = [
+                "--allow-overwrite/--no-allow-overwrite", "if_allow_overwrite",
+            ],
+            default = False,
+        ),
+    )
+
+    OPTIONS_INTERMEDIATE: typing.Tuple[click.Option, ...] = (
+        click.Option(
+            param_decls = [
+                "--intermediate/--no-intermediate", "if_gen_intermediate",
+            ],
+            default = False,
+            help = """When specified, 
+the program will generate
+intermediate products of the conversion process
+that facilitate development and debugging.""",
+        ),
+        click.Option(
+            param_decls = [
+                "--intermediate-dir", "intermediate_dir",
+            ],
+            type = click.Path(file_okay = False, dir_okay = True),
+            default = None,
+            help = """The path to intermediate logs.
+This option has no effect 
+unless the `--intermediate' flag is set on.""",
+        )
+    )
+
+    def __init__(
+        self,
+        name: str,
+        logger_orig: logging.Logger,
+        folder_prefix: str,
+        with_intermediate: bool,
+        callback_preprocessing: typing.Optional[
+#            typing.Callable[[dict, typing.Any], int]
+            typing.Callable
+        ],
+        callback_process_filelist: typing.Optional[
+            typing.Callable
+#            typing.Callable[
+#                [
+#                    dict, 
+#                    pathlib.Path, 
+#                    typing.Optional[pathlib.Path], 
+#                    FileList,
+#                    P.kwargs
+#                ], 
+#                int,
+#            ]
+        ],
+        callback_process_rawtrees: typing.Optional[
+#            typing.Callable[
+#                [dict, pathlib.Path,typing.Any], 
+#                int,
+#            ]
+            typing.Callable
+        ],
+        **kwargs
+    ):
+        if kwargs.get("callback"):
+            raise ValueError("Callback must be empty!")
+        # === END IF ===
+
+        super().__init__(name, **kwargs)
+        self.logger_orig = logger_orig
+        self.folder_prefix = folder_prefix
+        self.callback_preprocessing = callback_preprocessing
+        self.callback_process_filelist = callback_process_filelist
+        self.callback_process_rawtrees = callback_process_rawtrees
+
+        if with_intermediate:
+            self.params = [
+                *self.OPTIONS_NORMAL, 
+                *self.OPTIONS_INTERMEDIATE,
+            ]
+            self.callback = self._template_procedure
+        else:
+            self.params = list(self.OPTIONS_NORMAL)
+            self.callback = functools.partial(
+                self._template_procedure,
+                if_gen_intermediate = False,
+                intermediate_dir = None,
+            )
+        # === END IF ===
+    # === END ===
+
+    def _template_procedure(
+        self,
+        source_type: str,
+        destination: typing.Optional[str],
+        if_allow_overwrite: bool,
+        if_gen_intermediate: bool,
+        intermediate_dir: typing.Optional[str],
+        **kwargs,
+    ) -> None:
+        logger.info(
+            "Command template invoked; "
+            f"Template class: {self.__class__.__name__}, "
+            f"Command name: {self.name}"
+        )
+        ctx = click.get_current_context()
+        CONFIG = ctx.obj["CONFIG"]
+
+        if self.callback_preprocessing:
+            logger.info(
+                "Command template is calling the preprocessing callback"
+            )
+            res_code: int = self.callback_preprocessing(CONFIG, **kwargs)
+
+            if res_code:
+                logger.info(
+                    "The preprocessing callback ended up returning an error "
+                    f"(code: {res_code})"
+                )
+                ctx.exit(res_code)
+            # === END IF ===
+            logger.info(
+                "The preprocessing callback has succeeded"
+            )
+        # === END IF ===
+
+        if source_type == "filelist":
+            logger.info(
+                "The type of source: filelist"
+            )
+
+            if not self.callback_process_filelist:
+                return
+            # === END IF ===
+
+            dir_prefix_result: str = f"result_{self.folder_prefix}_"
+            dir_prefix_log: str = f"log_{self.folder_prefix}_"
+
+            res_folders: typing.Dict[str, pathlib.Path]
+        
+            res_folders = create_folder_time_multiple(
+                filter(
+                    None,
+                    (
+                        dir_prefix_result 
+                            if not destination else None, 
+                        dir_prefix_log 
+                            if if_gen_intermediate and not intermediate_dir
+                            else None,
+                    )
+                )
+            )
+            if destination:
+                result_conv_parsed = pathlib.Path(destination)
+                if result_conv_parsed.is_dir():
+                    # Check if the folder is empty or not
+                    pass
+                else: 
+                    result_conv_parsed.mkdir(parents = True)
+                # === END IF ===
+
+                res_folders[dir_prefix_result] = result_conv_parsed
+            else:
+                pass
+            # === END IF ===
+
+            if if_gen_intermediate and intermediate_dir:
+                intermediate_parsed = pathlib.Path(intermediate_dir)
+
+                if intermediate_parsed.is_dir():
+                    # Check if the folder is empty or not
+                    pass
+                else: 
+                    intermediate_parsed.mkdir(parents = True)
+                # === END IF ===
+
+                res_folders[dir_prefix_log] = intermediate_parsed
+            else:
+                pass
+            # === END IF ===
+
+            logger.info(
+                "The destination folders are created; "
+                f"Path to result files: {res_folders.get(dir_prefix_result, 'N/A')}, "
+                f"Path to intermediate files: {res_folders.get(dir_prefix_log, 'N/A')}"
+            )
+            # === END IF ===
+
+            flist: FileList = FileList.from_file_list(sys.stdin)
+
+            # ------------------------
+            # Execute the callback
+            # ------------------------
+            res_code = self.callback_process_filelist(
+                CONFIG, 
+                res_folders[dir_prefix_result],
+                res_folders.get(dir_prefix_log),
+                flist,
+                **kwargs,
+            )
+
+            if res_code:
+                ctx.exit(res_code)
+            # === END IF ===
+        elif source_type == "rawtrees":
+            # ------------------------
+            # Skip if no callback
+            # ------------------------
+            if not self.callback_process_rawtrees:
+                return 
+            # === END IF ===
+
+            # ------------------------
+            # Create an intermed folder if required
+            # ------------------------
+            log_pfx: typing.Optional[pathlib.Path]
+
+            if if_gen_intermediate:
+                if intermediate_dir:
+                    intermediate_dir_parsed = pathlib.Path(intermediate_dir)
+                    if intermediate_dir_parsed.is_dir():
+                    # Check if the folder is empty or not
+                        pass
+                    else: 
+                        intermediate_dir_parsed.mkdir(parents = True)
+                    # === END IF ===
+                    log_pfx = intermediate_dir_parsed / "rawtrees"
+                else:
+                    log_pfx = create_folder_time("log_conv") / "rawtrees"
+                # === END IF ===
+            else:
+                log_pfx = None
+            # === END IF ===
+
+            # ------------------------
+            # Execute the callback
+            # ------------------------
+            res_code = self.callback_process_rawtrees(
+                conf = CONFIG,
+                dir_log = log_pfx,
+                **kwargs
+            )
+
+            if res_code:
+                ctx.exit(res_code)
+            # === END ===
+        else:
+            self.logger_orig.error(
+                "Invalid CLI option: `source_type' must be either `stdin` or `filelist', "
+                f"but {source_type} is given."
+            )
+            raise ValueError()
+        # === END IF ===
+    # === END ===
--- abctk/conversion/cli.py
+++ abctk/conversion/cli.py
@@ -4,8 +4,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 import datetime
-import pathlib
+import functools
 import os
+import pathlib
+import sys
 
 import click
 
@@ -13,206 +15,75 @@ import abctk.cli_tool as ct
 import abctk.config as CONF
 from . import core
 
-@click.command()
-@click.option(
-    "-i", "--input", "--source", "source_type",
-    type = click.Choice(
-        choices = ("rawtrees", "filelist"),
-        case_sensitive = False
-    ),
-    default = "rawtrees",
-    help = """
-        \b
-        Designating your data source. 
-        - `rawtrees' means that input Keyaki trees are provided directry from STDIN.
-        - `filelist' indicates that STDIN provides a list of paths to Keyaki tree files.
-        Default to `stdin'. Case insensitive.
-    """
-)
-@click.option(
-    "-o", "--output", "--to", "--destination", "destination",
-    type = click.Path(file_okay = False, dir_okay = True),
-    help = """
-        Path to a folder that stores the resulting files.
-        Since this program yields results in multiple files in different formats, it is always needed to have a storage folder.
-        Default to `./result_(timestamp)`.
-    """
-)
-@click.option(
-    "-o", "--overwrite/--no-overwrite", "can_overwrite",
-    default = False,
-)
-@click.option(
-    "--intermediate/--no-intermediate", "if_gen_intermediate",
-    default = False,
-    help = """
-        When specified, the program will generate 
-            intermediate products of the conversion process
-            that facilitate development and debugging. 
-    """,
-)
-@click.option(
-    "--intermediate-dir", "intermediate_dir",
-    type = click.Path(file_okay = False, dir_okay = True),
-    default = None,
-    help = """
-        The path to intermediate logs.
-        This option has no effect unless the `--intermediate' flag is set on.
-    """,
-)
-@click.pass_context
-def cmd_main(
-    ctx: click.Context,
-    source_type: str,
-    destination: typing.Optional[str],
-    can_overwrite: bool,
-    if_gen_intermediate: bool,
-    intermediate_dir: typing.Optional[str],
-) -> None:
-    """
-        Convert Keyaki tree(s) to ABC trees.
-    """
-    CONFIG = ctx.obj["CONFIG"]
-    
-    # ================
-    # Check the system runtimes
-    # ================
-    import sys
+def _check_sys_runtimes(conf: dict, **kwargs) -> int:
+    return core.check_runtimes(conf["bin-sys"])
+# === END ===
 
-    runtime_check_res: int = core.check_runtimes(CONFIG["bin-sys"])
-    if runtime_check_res:
-        sys.exit(runtime_check_res)
-    else:
-        pass
-    # === END IF ===
+def _process_filelist(
+    conf: dict,
+    dir_result: pathlib.Path,
+    dir_log: typing.Optional[pathlib.Path],
+    flist: ct.FileList,
+    **kwargs,
+) -> int:
+    import multiprocessing as mp
+    import tqdm
 
-    # ================
-    # Conversion
-    # ================
-    if source_type == "filelist":
-        # Prepare the destination folder
-        res_folders: typing.Dict[str, pathlib.Path]
+    proc_num: int = conf["max_process_num"]
+    with mp.Pool(processes = proc_num) as pool:
+        logger.info(f"Number of processes: {proc_num}")
+
+        flist_dest_expanded: typing.List[KeyakiFileConversionArgs] = list(
+            KeyakiFileConversionArgs(
+                src = src,
+                dest = dir_result / fn,
+                size = os.path.getsize(src),
+                conf = conf,
+                log_prefix = (
+                    dir_log / fn
+                    if dir_log
+                    else None
+                ),
+            )
+            for src, fn 
+            in flist.iterate_truncating_commonprefix()
+        )
+        files_total_size: int = sum(
+            map(lambda x: x[2], flist_dest_expanded)
+        )
         
-        res_folders = ct.create_folder_time_multiple(
-            filter(
-                None,
-                (
-                    "result_conv_" if not destination else None, 
-                    "log_conv_" 
-                    if if_gen_intermediate and not intermediate_dir
-                    else None,
-                )
-            )
+        jobs = pool.imap_unordered(
+            __conv_file_wrapper,
+            flist_dest_expanded,
         )
-        if destination:
-            result_conv_parsed = pathlib.Path(destination)
-            if result_conv_parsed.is_dir():
-                # Check if the folder is empty or not
-                pass
-            else: 
-                result_conv_parsed.mkdir(parents = True)
-            # === END IF ===
+        with tqdm.tqdm(
+            total = files_total_size, 
+            unit = "B",
+            unit_scale = True,
+            unit_divisor = 1024
+        ) as pb:
+            pb.write("Converting Keyaki trees into ABC trees:")
+            for _return_code, src_size in jobs:
+                pb.update(src_size)
+        # === END WITH pb ===
+    # === END FOR path_src, filename_res ===
 
-            res_folders["result_conv_"] = result_conv_parsed
-        else:
-            pass
-        # === END IF ===
+    return 0
+# === END ===
 
-        if if_gen_intermediate and intermediate_dir:
-            intermediate_parsed = pathlib.Path(intermediate_dir)
-
-            if intermediate_parsed.is_dir():
-                # Check if the folder is empty or not
-                pass
-            else: 
-                intermediate_parsed.mkdir(parents = True)
-            # === END IF ===
-
-            res_folders["log_conv_"] = intermediate_parsed
-        else:
-            pass
-        # === END IF ===
-
-        flist: ct.FileList = ct.FileList.from_file_list(sys.stdin)
-
-        import multiprocessing as mp
-        import tqdm
-
-        proc_num: int = CONFIG["max_process_num"]
-        with mp.Pool(processes = proc_num) as pool:
-            logger.info(f"Number of processes: {proc_num}")
-
-            log_prefix = res_folders.get("log_conv_")
-
-            flist_dest_expanded: typing.List[KeyakiFileConversionArgs] = list(
-                KeyakiFileConversionArgs(
-                    src = src,
-                    dest = res_folders["result_conv_"] / fn,
-                    size = os.path.getsize(src),
-                    conf = CONFIG,
-                    log_prefix = (
-                        log_prefix / fn
-                        if log_prefix
-                        else None
-                    ),
-                )
-                for src, fn 
-                in flist.iterate_truncating_commonprefix()
-            )
-            files_total_size: int = sum(
-                map(lambda x: x[2], flist_dest_expanded)
-            )
-            
-            jobs = pool.imap_unordered(
-                __conv_file_wrapper,
-                flist_dest_expanded,
-            )
-            with tqdm.tqdm(
-                total = files_total_size, 
-                unit = "B",
-                unit_scale = True,
-                unit_divisor = 1024
-            ) as pb:
-                pb.write("Converting Keyaki trees into ABC trees:")
-                for _return_code, src_size in jobs:
-                    pb.update(src_size)
-            # === END WITH pb ===
-        # === END FOR path_src, filename_res ===
-    elif source_type == "rawtrees":
-        log_pfx: typing.Optional[pathlib.Path]
-
-        if if_gen_intermediate:
-            if intermediate_dir:
-                intermediate_dir_parsed = pathlib.Path(intermediate_dir)
-                if intermediate_dir_parsed.is_dir():
-                # Check if the folder is empty or not
-                    pass
-                else: 
-                    intermediate_dir_parsed.mkdir(parents = True)
-                # === END IF ===
-                log_pfx = intermediate_dir_parsed / "rawtrees"
-            else:
-                log_pfx = ct.create_folder_time("log_conv") / "rawtrees"
-            # === END IF ===
-        else:
-            log_pfx = None
-        # === END IF ===
-
-        _ = core.convert_keyaki_to_abc(
-            f_src = sys.stdin,
-            f_dest = sys.stdout,
-            src_name = "<STDIN>",
-            dest_name = "<STDOUT>",
-            conf = CONFIG,
-            log_prefix = log_pfx,
-        )
-    else:
-        logger.error(
-            "Invalid CLI option: `source_type' must be either `stdin` or `filelist', "
-            f"but {source_type} is given."
-        )
-        raise ValueError()
-    # === END IF ===
+def _process_rawtrees(
+    conf: dict,
+    dir_log: typing.Optional[pathlib.Path],
+    **kwargs,
+) -> int:
+    return core.convert_keyaki_to_abc(
+        f_src = sys.stdin,
+        f_dest = sys.stdout,
+        src_name = "<STDIN>",
+        dest_name = "<STDOUT>",
+        conf = conf,
+        log_prefix = dir_log,
+    )
 # === END ===
 
 class KeyakiFileConversionArgs(typing.NamedTuple):
@@ -249,3 +120,14 @@ def __conv_file_wrapper(
         args.size
     )
 # === END ===
+
+cmd_main = ct.CmdTemplate_Batch_Process_on_Tree(
+    name = "conv",
+    logger_orig = logger,
+    folder_prefix = "conv",
+    with_intermediate = True,
+    callback_preprocessing = _check_sys_runtimes,
+    callback_process_filelist = _process_filelist,
+    callback_process_rawtrees = _process_rawtrees,
+)
+cmd_main.__doc__ = "Convert Keyaki tree(s) to ABC trees."
