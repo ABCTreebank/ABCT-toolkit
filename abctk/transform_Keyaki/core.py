84696b2fb4142ead020d30f6b912683a7da418ee
Nori Hayashi <net@hayashi-lin.net>
2021-03-04 00:29:58 +0900

    [add] a miniature de-obfuscate function
    with tests

--- abctk/transform_Keyaki/core.py
+++ abctk/transform_Keyaki/core.py
@@ -79,3 +79,83 @@ def obfuscate_file(
         logger.exception(f"Exception {e} occurred when processing the file: {src}")
         raise
 # === END ===
+
+@attr.s(
+    auto_attribs = True
+)
+class UnmatchingDecriptionException(Exception):
+    amount: int
+
+def decrypt_stream(
+    f_src: typing.TextIO,
+    f_orig: typing.TextIO,
+    f_dest: typing.TextIO,
+    src_name: typing.Any = "<INPUT>",
+    orig_name: typing.Any = "<SOURCE>",
+    dest_name: typing.Any = "<OUTPUT>",
+    **kwargs
+) -> int:
+    def _gen(f_src, f_orig):
+        char_src : str
+        char_orig : str
+        while True:
+            char_src = f_src.read(1)
+
+            if char_src == "⛔":
+                char_orig = f_orig.read(1)
+                if char_orig:
+                    yield char_orig
+                else:
+                    raise UnmatchingDecriptionException(amount = -1)
+                    # TODO: get the amount
+            elif char_src == '':
+                char_orig = f_orig.read(1)
+                if char_orig:
+                    raise UnmatchingDecriptionException(amount = 1)
+                    # TODO: get the amount
+                else:
+                    # Success
+                    break # END
+            else:
+                yield char_src
+            # === END IF ===
+
+    res: str
+    try:
+        res = "".join(_gen(f_src, f_orig))
+    except UnmatchingDecriptionException as e:
+        logger.warning(
+            f"""The source tree file does not match with the original corpus.
+The tree file exceeds by {e.amount} char(s).
+Source: {src_name}
+Original: {orig_name}
+Output: {dest_name}
+The result, which is broken, is nevertheless dumped to {dest_name}."""
+        )
+    finally:
+        f_dest.write(res)
+
+    return 0
+# === END ===
+
+def decrypt_file(
+    src: pathlib.Path,
+    orig: pathlib.Path,
+    dest: pathlib.Path,
+    **kwargs
+) -> int:
+    try:
+        with open(src, "r") as h_src, open(orig, "r") as h_orig, open(dest, "w") as h_dest:
+            res = decrypt_stream(
+                h_src, h_orig, h_dest,
+                src, orig, dest,
+            )
+        # === END WITH h_src, h_orig, h_dest ===
+        return res
+    except IOError:
+        logger.exception(f"IO Error occurred when processing the file: {src}")
+        raise
+    except Exception as e:
+        logger.exception(f"Exception {e} occurred when processing the file: {src}")
+        raise
+# === END ===
