572cdc67931dbe29422e72914fc58314cd12515f
Nori Hayashi <net@hayashi-lin.net>
2021-03-30 20:05:43 +0900

    [add] classes and their parsers in types.real
    along with tests

--- abctk/types/real.py
+++ abctk/types/real.py
@@ -1,7 +1,8 @@
 import enum
-from functools import lru_cache
+import functools
 import itertools
 from packaging.version import Version
+import re
 #import sys
 #PY_VER = sys.version_info
 #
@@ -13,6 +14,7 @@ import typing
 # === END IF ===
 
 import attr
+import parsy
 
 from . import core
 
@@ -20,7 +22,7 @@ _X = typing.TypeVar("_X")
 _X1 = typing.TypeVar("_X1")
 _X2 = typing.TypeVar("_X2")
 
-NT      = typing.TypeVar("NT", typing.Any, core.IPrettyPrintable)
+NT = typing.TypeVar("NT", str, core.IPrettyPrintable)
 
 class ABCDepMarking(enum.Enum):
     """
@@ -97,19 +99,53 @@ class ABCCatPlus(
     def __str__(self) -> str:
         return self.pprint()
 
-    @staticmethod
-    @lru_cache()
-    def gen_larkgrammar(gram_nonterm: str) -> str:
-        rf"""
-        
-
-        {gram_nonterm.spilt()}
-
-        """
 # === END CLASS ===
 
 KeyakiCat = str
 KeyakiCat_ABCCatPlus = ABCCatPlus[KeyakiCat] # type: ignore
+
+
+@parsy.generate
+def parser_ABCCatPlus_AttrVal() -> parsy.Parser:
+    yield parsy.string("#")
+    attr = yield parsy.regex(r"[^=\s]+")
+    yield parsy.string("=")
+    if attr == "role":
+        val = yield parsy.from_enum(ABCDepMarking)
+    else:
+        val = yield parsy.regex(r"[^#\s]*")
+    # === END IF ===
+    
+    return (attr, val)
+# === END ===
+
+@functools.lru_cache()
+def parser_ABCCatPlus(
+    parser_cat: parsy.Parser = parsy.regex(r"[^#\s]*")
+) -> parsy.Parser:
+    @parsy.generate
+    def _parser():
+        cat = yield parser_cat
+        attrvals = yield parser_ABCCatPlus_AttrVal.many().map(dict)
+
+        role: ABCDepMarking = attrvals.pop("role", ABCDepMarking.NONE)
+        deriv: str = attrvals.pop("deriv", "")
+
+        res: ABCCatPlus = ABCCatPlus(
+            cat = cat,
+            role = role,
+            deriv = deriv,
+        )
+        
+        for key, val in attrvals.items():
+            setattr(res, key, val)
+        # === END FOR key, val ===
+    
+        return res
+    # === END ===
+
+    return _parser
+# === END ===
 
 class ABCCatBot(enum.Enum):
     """
@@ -179,22 +215,92 @@ ABCCat_ABCCatPlus = ABCCatPlus[ABCCat]
 KeyakiTree            = core.TypedTree[KeyakiCat, str]
 KeyakiTree_ABCCatPlus = core.TypedTree[KeyakiCat_ABCCatPlus, str]
 ABCTree_ABCCatPlus    = core.TypedTree[ABCCat_ABCCatPlus, str]
+parser_ABCCatBot: parsy.Parser = parsy.from_enum(ABCCatBot)
+parser_ABCCatBase: parsy.Parser = parsy.regex(r"[^/\\<>#\s]+")
 
-@lru_cache()
-def gen_larkgrammar_PennTreebank(
-) -> str:
-    return rf"""
-%import common.WS
-%ignore common.WS
+@parsy.generate
+def parser_ABCCatFunctor_Left() -> parsy.Parser: 
+    que = yield (
+        parser_ABCCatBot
+        | parser_ABCCatBase
+        | parser_ABCCat_group
+    ).sep_by(
+        parsy.string("\\"),
+        min = 2,
+    )
 
-tree_simple: TERM
-tree_complex: "(" NONTERM? tree* ")"
-tree: tree_simple | tree_complex
+    return functools.reduce(
+        lambda c, a: ABCCatFunctor(
+            mode = ABCCatFunctorMode.LEFT,
+            ant = a,
+            conseq = c,
+        ),
+        reversed(que)
+    )
+# === END ===
 
-tree_with_id: "(" tree_complex "(" "ID" /[^\s]+/ ")" ")"
-tree_maybe_with_id: tree_with_id | tree
-tree_list: tree_maybe_with_id*
+@parsy.generate
+def parser_ABCCatFunctor_Right() -> parsy.Parser:
+    stack = yield (
+        parser_ABCCatBot
+        | parser_ABCCatBase
+        | parser_ABCCatFunctor_Left
+        | parser_ABCCat_group
+    ).sep_by(
+        parsy.string("/"),
+        min = 2,
+    )
 
-NONTERM: /[^\s]+/
-TERM: /[^\s]+/
-"""
+    return functools.reduce(
+        lambda a, c: ABCCatFunctor(
+            mode = ABCCatFunctorMode.RIGHT,
+            ant = a,
+            conseq = c,
+        ),
+        stack
+    )
+# === END ===
+
+parser_ABCCat_simple: parsy.Parser = (
+    parser_ABCCatFunctor_Left
+    | parser_ABCCatFunctor_Right
+    | parser_ABCCatBot 
+    | parser_ABCCatBase
+)
+
+parser_ABCCat_group: parsy.Parser = (
+    parsy.string("<")
+    >> parser_ABCCat_simple
+    << parsy.string(">")
+)
+
+parser_ABCCat: parsy.Parser = (
+    parser_ABCCat_group 
+    | parser_ABCCat_simple
+)
+
+_path_ID_matcher = re.compile(
+    r"^(?P<ID>[0-9]+)"
+    r"_(?P<category>[^;_]+)"
+    r"_(?P<file_name>[^;])"
+    r";"
+)
+
+def make_path_from_Keyaki_or_ABC_ID(
+    ID: str, 
+    suffix: str = ".psd"
+) -> typing.Tuple[str, str]:
+    global _path_ID_matcher
+    
+    match = _path_ID_matcher.match(ID)
+    if match:
+        return (
+            f"{match.group('category')}"
+            f"_{match.group('file_name')}"
+            f"{suffix}",
+            ID
+        )
+    else:
+        return (f"untitled{suffix}", ID)
+    # === END IF ===
+# === END ===
