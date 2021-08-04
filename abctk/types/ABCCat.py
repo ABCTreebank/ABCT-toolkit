9d44bdddac46d265f3910a779a07e6f3a02b7054
Nori Hayashi <net@hayashi-lin.net>
2021-08-04 16:26:28 +0900

    [delete] redundant methods
    `ABCCat.apply` is already compatible with str args

--- abctk/types/ABCCat.py
+++ abctk/types/ABCCat.py
@@ -76,6 +76,11 @@ def parse_annot(source: str) -> Plain_Annot:
         The category part.
     remainder : dict
         The feature part.
+
+    Examples
+    --------
+    >>> parse_annot("<NP/NP>#role=h#deriv=leave")
+    ('<NP/NP>', {'role': <DepMk.HEAD: 'h'>, 'deriv': 'leave'})
     """
     match = _annot_cat_basic_matcher.match(source)
 
@@ -99,6 +104,13 @@ def annot_feat_pprint(
 ) -> str:
     """
     Prettyprint an ABC Treebank feature bundle.
+
+    Examples
+    --------
+    >>> _, feat_set = parse_annot("<NP/NP>#role=h#deriv=leave")
+    >>> annot_feat_pprint(feat_set)
+    '#role=h#deriv=leave'
+
     """
     if "role" in feat_set:
         role = f"#role={feat_set['role'].value}"
@@ -122,8 +134,16 @@ class ABCCatFunctorMode(Enum):
     """
     The enumeration of the types in ABC categories.
     """
+
     LEFT = "L"
+    """
+    The left functor `\\`.
+    """
+
     RIGHT = "R"
+    """
+    The right functor '/'.
+    """
 
     def __invert__(self):
         if self == self.LEFT:
@@ -145,20 +165,50 @@ class ABCCatReprMode(Enum):
     frozen = True,
 )
 class ABCReductionRes:
+    """
+    Representing details of simplification of ABC categories.
+    """
+
     func_mode: ABCCatFunctorMode
+    """
+    The used rule in the simplification.
+    """
+
     level: int
+    """
+    The depth of functional compoisition.
+    """
 
     def __str__(self):
         return f"{self.func_mode.value}{self.level}"
 
 ABCCatReady = typing.Union[str, "ABCCat"]
+"""
+The union of the types that is interpretable as ABC categories.
+"""
+
 class ABCCat():
     """
     The abstract base class representing ABC categories.
+
+    Notes
+    -----
+    All instances of all subclasses of this are expected to be identifiable by `==`, hashable, and frozen (unchangeable; all operations create new instances).
+
+    All category-related arguments of the methods are expected to accept both `ABCCat` and any other types that can be interpretable as `ABCCat`, instances of the latter types begin implicitly parsed.
+    This feature will enable a readable and succint coding and manupilation of `ABCCat` objects.
     """
 
     @functools.lru_cache()
     def adjunct(self, func_mode: ABCCatFunctorMode) -> "ABCCatFunctor":
+        """
+        Make a self adjunction from a category.
+
+        Examples
+        --------
+        >>> ABCCat.p("NP").adjunct(ABCCatFunctorMode.RIGHT).pprint()
+        '<NP/NP>'
+        """
         return ABCCatFunctor(
             func_mode = func_mode,
             ant = self,
@@ -166,13 +216,46 @@ class ABCCat():
         )
 
     def adj_l(self):
+        """
+        Make a self left adjunction from a category.
+        This is no more than an alias of `ABCCat.adjunct(cat, ABCCatFunctorMode.LEFT)`.
+        """
+
         return self.adjunct(ABCCatFunctorMode.LEFT)
 
     def adj_r(self):
+        """
+        Make a self right adjunction from a category.
+        This is no more than an alias of `ABCCat.adjunct(cat, ABCCatFunctorMode.RIGHT)`.
+        """
         return self.adjunct(ABCCatFunctorMode.RIGHT)
 
     @functools.lru_cache()
     def r(self, ant: ABCCatReady) -> "ABCCatFunctor":
+        """
+        An iconic method to create a right functor cateogry.
+        To the argument comes the antecedent (viz. the `B` in `A/B`).
+
+        Notes
+        -----
+        The (antecedent) argument can be of any types in `ABCCatReady`.
+        It is not necessary to convert an `str` antecedent into an `ABCCat` beforehand.
+
+        The `/` (true division) operator is also available as an alias of this function.
+
+        See also
+        --------
+        ABCCat.l:
+            The same method for left functors.
+
+        Examples
+        --------
+        >>> ABCCat.p("NP").r("Scomp").pprint()
+        '<NP/Scomp>'
+
+        >>> (ABCCat.p("NP") / "Scomp").pprint()
+        '<NP/Scomp>'
+        """
         return ABCCatFunctor(
             func_mode = ABCCatFunctorMode.RIGHT,
             ant = self.p(ant),
@@ -184,6 +267,23 @@ class ABCCat():
 
     @functools.lru_cache()
     def l(self, conseq: ABCCatReady) -> "ABCCatFunctor":
+        """
+        An iconic method to create a left functor cateogry.
+        To the argument comes the consequence (viz. the `B` in `B\\A`).
+
+        Notes
+        -----
+        The (antecedent) argument can be of any types in `ABCCatReady`.
+        It is not necessary to convert an `str` antecedent into an `ABCCat` beforehand.
+
+        For left functors, which has no baskslash counterpart that is eligible for an Python binary operator (like the `/` for right functors),
+            a workaround would be combining `/` with the direction inversion `~`.
+
+        Examples
+        --------
+        >>> ABCCat.p("NP").l("S").pprint()
+        '<NP\\\\S>'
+        """
         return ABCCatFunctor(
             func_mode = ABCCatFunctorMode.LEFT,
             ant = self,
@@ -203,6 +303,50 @@ class ABCCat():
         left: ABCCatReady,
         right: ABCCatReady
     ) -> typing.Tuple["ABCCat", typing.Optional[ABCReductionRes]]:
+        """
+        Simplify two ABC categories.
+
+        Arguments
+        ---------
+        left: ABCCatReady
+        right: ABCCatReady
+
+        Returns
+        -------
+        cat: ABCCat
+            The resulting category.
+            If simplification fails, it yields `ABCCatBot.BOT` (the bottom ⊥).
+        res: ABCReductionRes or None
+            The details of the simplification process.
+            If it fails, it is a `None`.
+
+        Notes
+        -----
+        `*` is an alias operator with simplification details omitted.
+
+        Examples
+        --------
+        >>> cat, res = ABCCat.apply("NP", "NP\\\\S")
+        >>> cat.pprint()
+        'S'
+        >>> str(res)
+        'L0'
+        >>> cat == ABCCat.p("NP") * "NP\\\\S"
+        True
+
+        >>> cat, res = ABCCat.apply("C/S", "S/NP")
+        >>> cat.pprint()
+        '<C/NP>'
+        >>> str(res)
+        'R1'
+
+        >>> cat, res = ABCCat.apply("S", "NP")
+        >>> cat.pprint()
+        '⊥'
+        >>> res is None
+        True
+        """
+
         left_parsed: "ABCCat" = cls.p(left)
         right_parsed: "ABCCat" = cls.p(right)
 
@@ -330,19 +474,6 @@ class ABCCat():
         else:
             raise TypeError
 
-    @classmethod
-    @functools.lru_cache()
-    def apply_str(
-        cls, left: str, right: str
-    ) -> typing.Tuple["ABCCat", typing.Optional[ABCReductionRes]]:
-        """
-        Reduce a pair of ABC categories in string forms.
-        """
-        return cls.apply(
-            left = cls.parse(left),
-            right = cls.parse(right),
-        )
-
     def __mul__(self, others):
         res, _ = ABCCat.apply(self, others)
         return res
@@ -362,20 +493,48 @@ class ABCCat():
     ):
         """ 
         Parse an ABC category.
-        """
-        if mode == ABCCatReprMode.TLCG:
-            return cls.p(source)
-        else:
-            return cls.p_trad(source)
 
-    @classmethod
-    def p(cls, source: ABCCatReady) -> "ABCCat":
+        Arguments
+        ---------
+        source: ABCCatReady
+            The thing to be parsed. If it is already an `ABCCat`, nothing happens and the method just returns this thing.
+        mode: ABCCatReprMode, default: ABCCatReprMode.TLCG
+            The linear order of functor categories.
+
+        Returns
+        -------
+        parsed: ABCCat
+
+        Examples
+        --------
+        >>> np: ABCCatBase = ABCCat.parse("NP")
+        >>> np.name
+        'NP'
+
+        >>> pred: ABCCatFunctor = ABCCat.parse("NP\\\\S")
+        >>> pred.pprint()
+        '<NP\\\\S>'
+
+        >>> ABCCat.parse(pred) == pred
+        True
+        """
         if isinstance(source, str):
             return _parser_ABCCat.parse(source) # typing: ignore
         elif isinstance(source, ABCCat):
             return source
         else:
             raise TypeError
+
+    @classmethod
+    def p(
+        cls,
+        source: ABCCatReady,
+        mode: ABCCatReprMode = ABCCatReprMode.TLCG,
+    ) -> "ABCCat":
+        """
+        An alias of `ABCCat.parse`.
+        """
+        return cls.parse(source, mode)
 
     @classmethod
     def p_trad(cls, source: ABCCatReady):
@@ -411,7 +570,14 @@ class ABCCatBot(ABCCat, Enum):
     hash = True,
 )
 class ABCCatBase(ABCCat):
+    """
+    Representing atomic ABC categories.
+    """
+
     name: str
+    """
+    The letter of the atom.
+    """
 
     def pprint(
         self, 
@@ -438,9 +604,24 @@ class ABCCatBase(ABCCat):
     hash = True, # Implemented automatically
 )
 class ABCCatFunctor(ABCCat):
+    """
+    Representing functor categories.
+    """
+
     func_mode: ABCCatFunctorMode
+    """
+    The mode, or direction, of the functor.
+    """
+
     ant: "ABCCat"
+    """
+    The antecedent.
+    """
+
     conseq: "ABCCat"
+    """
+    The consequence.
+    """
 
     def pprint(
         self, 
@@ -458,6 +639,23 @@ class ABCCatFunctor(ABCCat):
     # === END ===
 
     def invert_dir(self):
+        """
+        Invert the direction of the functor.
+
+        Notes
+        -----
+        This method always returns a new instance.
+
+        Examples
+        --------
+        >>> cat = ABCCat.p("NP\\\\S")
+        >>> cat.invert_dir().pprint()
+        '<S/NP>'
+
+        >>> cat.invert_dir() == ~cat
+        True
+        """
+
         fm_new = ~self.func_mode
         return attr.evolve(
             self,
