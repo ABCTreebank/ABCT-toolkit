c51ffbb8a97b9832c74654e38e25e69c4cd7a6a4
Nori Hayashi <net@hayashi-lin.net>
2021-12-05 17:17:33 +0900

    [change] improve tweak CLI
    - STDIN support
    - function dictionary

--- abctk/cli_typer/tweak.py
+++ abctk/cli_typer/tweak.py
@@ -1,7 +1,11 @@
 import logging
 logger = logging.getLogger(__name__)
+import os
 import pathlib
+import sys
+import tempfile
 import typing
+
 
 import fs
 from tqdm.auto import tqdm
@@ -15,65 +19,56 @@ import abctk.transform_ABC.norm as norm
 from abctk.transform_ABC.elim_empty import elim_empty_terminals
 from abctk.transform_ABC.elim_trace import restore_rel_trace
 
-def parse_command(command: str) -> typing.Callable[[Tree, str], typing.Any]:
-        com_args = command.split(".")
-        com_name, others = com_args[0], com_args[1:]
-        
-        if com_name == "bin-conj":
-            if not others:
-                logger.info("Command: binarize conjunctions")
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "flatten-conj":
-            if not others:
-                logger.info("Command: flatten conjunctions")
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "elim-empty":
-            if not others:
-                logger.info("Command: eliminate empty nodes")
-                return elim_empty_terminals
-            else:
-                raise ValueError
-        elif com_name == "elab-empty":
-            if not others:
-                logger.info("Command: restore empty nodes")
-                return restore_rel_trace
-            else:
-                raise ValueError
-        elif com_name == "elim-trace":
-            if not others:
-                logger.info("Command: eliminate relative clause traces")
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "elab-trace":
-            if not others:
-                logger.info("Command: restore relative clause traces")
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "janome":
-            if not others:
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "del-janome":
-            if not others:
-                raise NotImplementedError
-            else:
-                raise ValueError
-        elif com_name == "min-nodes":
-            raise NotImplementedError
-        elif com_name == "elab-nodes":
-            raise NotImplementedError
-        elif com_name == "obfus":
-            raise NotImplementedError
-        else:
-            raise ValueError
-
+TreeFunc = typing.Callable[
+    [Tree, str],
+    typing.Any
+]
+_COMMAND_TABLE: typing.Dict[str, typing.Tuple[TreeFunc, str]] = {
+    "bin-conj": (
+        lambda tree, ID: NotImplemented,
+        "binarize conjunctions",
+    ),
+    "flatten-conj": (
+        lambda tree, ID: NotImplemented,
+        "flatten conjunctions"
+    ),
+    "elim-empty": (
+        elim_empty_terminals,
+        "eliminate empty nodes"
+    ),
+    "restore-empty": (
+        lambda tree, ID: NotImplemented,
+        "restore empty nodes"
+    ),
+    "elim-trace": (
+        lambda tree, ID: NotImplemented,
+        "eliminate traces",
+    ),
+    "restore-trace": (
+        restore_rel_trace,
+        "restore traces",
+    ),
+    "janome": (
+        lambda tree, ID: NotImplemented,
+        "add janome morphological analyses",
+    ),
+    "del-janome": (
+        lambda tree, ID: NotImplemented,
+        "delete janome morphological analyses",
+    ),
+    "min-nodes": (
+        lambda tree, ID: NotImplemented,
+        "minimize nodes",
+    ),
+    "elab-nodes": (
+        lambda tree, ID: NotImplemented,
+        "elaborate nodes",
+    ), 
+    "obfus": (
+        lambda tree, ID: NotImplemented,
+        "obfuscate trees",
+    )
+}
 app = typer.Typer()
 
 @app.callback()
@@ -109,23 +104,88 @@ def cmd_from_treebank(
 
     # parse commands
     logger.info("Start parsing commands")
-    func_list = tuple(parse_command(com) for com in commands)
 
     # load trees
     tb = list(nt.load_ABC_psd(source_path))
 
     for ID, tree in tqdm(tb, desc = "Tweaking ABC trees"):
-        for f in func_list:
+        for com in commands:
+            func, desc = _COMMAND_TABLE[com]
+            
+            logger.info(f"Command: {desc}")
             # apply tweaks
-            f(tree, ID)
+            func(tree, ID)
 
     with fs.open_fs(str(dest_path), create = True) as folder:
         nt.dump_ABC_to_psd(tb, folder)
         
-@app.command("stdin")
-def cmd_from_stdin():
-    """
-    
-    """
+@app.command("file")
+def cmd_from_file(
+    source_path: pathlib.Path = typer.Argument(
+        ...,
+        file_okay = True,
+        dir_okay = False,
+        allow_dash = True,
+        help = """
+        The path to the input file. `-` indicates STDIN.
+        """
+    ),
+    dest_path: pathlib.Path = typer.Argument(
+        ...,
+        file_okay = True,
+        dir_okay = False,
+        allow_dash = True,
+        help = """
+        The destination. `-` indicates STDOUT.
+        """
+    ),
+    commands: typing.List[str] = typer.Argument(
+        ...,
+        help = """
+        A list of commands to execute upon each tree.
+        """
+    )
+):
+    with tempfile.TemporaryDirectory(
+        prefix = "abct_tweak_"
+    ) as temp_folder:
+        source_file: typing.Optional[typing.IO[str]] = None
+        dest_file: typing.Optional[typing.IO[str]] = None
+        try:
+            if source_path.name == "-":
+                source_file = tempfile.NamedTemporaryFile("w", dir = temp_folder)
+                source_file.write(sys.stdin.read())
+                logger.info(f"STDIN loaded in {source_file.name}")
+            else:
+                source_path = source_path.resolve()
+                source_file_path = f"{temp_folder}/{source_path.name}"
+                os.symlink(source_path, dst = source_file_path)
+                logger.info(f"File symlinked to {source_file_path}")
 
-    raise NotImplementedError
+            tb = list(nt.load_ABC_psd(temp_folder, re_filter = ".*"))
+
+            if dest_path.name == "-":
+                dest_file = sys.stdout
+            else:
+                dest_file = open(str(dest_path.resolve()), "w")
+
+            for ID, tree in tqdm(tb, desc = "Tweaking ABC trees"):
+                for com in commands:
+                    func, desc = _COMMAND_TABLE[com]
+                    
+                    logger.info(f"Command: {desc}")
+                    # apply tweaks
+                    func(tree, ID)
+
+                # dump the tree
+                dest_file.writelines(
+                    (
+                        nt.flatten_tree_with_ID(
+                            ID, tree
+                        ),
+                        "\n",
+                    )
+                )
+        finally:
+            if source_file: source_file.close()
+            if dest_file: dest_file.close()
